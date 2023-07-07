420384ae207ed3a302330f70c6acffcda52dbcee
Алексей Коковин <KokovinAA@cross-d.ru>
2023-07-07 10:57:53 +0300

    contacts

--- src/components/contact-tile.vue
+++ src/components/contact-tile.vue
@@ -1,15 +1,20 @@
 <template>
   <cd-tile>
     <div class="h5 text-start">Связаться с нами</div>
-    <form>
-      <div class="form-floating mb-3 sc">
-        <input type="email" class="form-control" id="floatingInput" placeholder="Имя и отчество">
-        <label for="floatingInput">Имя и отчество</label>
+    <form class="w-50 mx-auto" action="mailto:info@cross-d.ru" method="post" enctype="text/plain">
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" name="subject" id="subject" placeholder="Имя и отчество">
+        <label for="subject">Имя и отчество</label>
       </div>
-      <div class="form-floating mb-3 sc">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Ваша электронная почта</label>
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+        <label for="email">Ваша электронная почта</label>
       </div>
+      <div class="form-floating mb-3">
+        <textarea class="form-control" name="body" id="body"></textarea>
+        <label for="body">Ваш вопрос</label>
+      </div>
+      <input class="btn btn-primary" type="submit" value="Отправить"/>
     </form>
   </cd-tile>
 </template>
